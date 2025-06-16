2756e2a26de89c01b8c5c5e51c45dd3250fff19b
Tanushree Dawande <tanudawande54@gmail.com>
2025-06-17 00:10:10 +0530

    added some functionality

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,47 +1,57 @@
-import React from 'react'
+
+import React, { useState } from 'react'
+
 import './Sidebar.css'
 import{assets} from  '../../assets/assets'
 
 const Sidebar = () => {
+  const[extended , setExtended] = useState(false);
+
   return (
     <div className='sidebar'> 
       <div className="top">
-      <img className='menu' src={assets.menu_icon} alt="" />
-      <div className="new-chat">
-        <img src={assets.plus_icon} alt="" />
-        <p>New Chat</p>
-        <div className="recent">
+       <img onClick = {() =>setExtended(prev=>!prev)}className='menu' src={assets.menu_icon} alt="" />
+       <div className="new-chat">
+         <img src={assets.plus_icon} alt="" />
+         {extended ?<p>New Chat</p>:null}
+        </div>
+
+        {extended
+         ?<div className="recent">
           <p className="recent-title">Recent</p>
           <div className="recent-entry">
             <img src={assets.message_icon} alt="" />
             <p>What is react ...</p>
           </div>
-        </div>
+         </div>
+         :null
+        }
+
+        
       </div>
+    
       <div className="bottom">
         <div className="bottom-item">
           <img src={assets.question_icon} alt="" />
-          <p>Help</p>
-
+          {extended?<p>Help</p>:null}
         </div>
 
-         <div className="bottom-item">
+        <div className="bottom-item">
           <img src={assets.history_icon} alt="" />
-          <p>Activity</p>
-
+          {extended?<p>Activity</p>:null}
         </div>
 
 
-         <div className="bottom-item">
-          <img src={assets.send_icon} alt="" />
-          <p>Settings</p>
+        <div className="bottom-item">
+          <img src={assets.setting_icon} alt="" />
+          {extended?<p>Settings</p>:null}
 
         </div>
 
       </div>
       
-      </div>
     </div>
+   
   )
 }
 
